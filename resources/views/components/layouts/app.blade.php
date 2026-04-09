c53827e2af90653d13a4fd05a52a88eb76f396ed
Aiden McCaughey <a.mccaughey@ulster.ac.uk>
2026-04-09 18:21:08 +0100

    remove hand coded app name

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Blade Kit</title>
+      <title>{{ config('app.name') }}</title>
       <link href="https://fonts.bunny.net/css?family=inter:400,500,600" rel="stylesheet" />      
       @vite(['resources/css/app.css', 'resources/js/app.js'])     
       @livewireStyles
